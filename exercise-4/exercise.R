e6c02782ea9c29202c9af22d5c999e01717b704c
Kelly Chhor <kcc343@gmail.com>
2018-04-02 22:30:55 -0700

    do exercises for functions

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -5,11 +5,24 @@
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
 
+is_twice_as_long <- function(first_string, second_string) {
+  minimum_length <- min(nchar(first_string), nchar(second_string))
+  maximum_length <- max(nchar(first_string), nchar(second_string))
+  same_length <- maximum_length >= (2 * minimum_length)
+  return(same_length)
+}
+
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
 
+first <- "Hello"
+second <- "ljljklklkl"
+test <- is_twice_as_long(first, second)
+first_test <- "Bonjour"
+second_test <- "Hi"
+test2 <- is_twice_as_long(first_test, second_test)
 
 
 # Define a function `describe_difference` that takes in two strings. The
@@ -18,7 +31,27 @@
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
 
+describe_difference <- function(first_string, second_string) {
+  if (nchar(first_string) > nchar(second_string)) {
+    difference = nchar(first_string) - nchar(second_string)
+    status = paste("Your first string is longer by", difference, "characters")
+  } else if (nchar(first_string) < nchar(second_string)) {
+    difference = nchar(second_string) - nchar(first_string)
+    status = paste("Your second string is longer by", difference, "characters")
+  } else {
+    status = "Your strings are the same length!"
+  }
+  return(status)
+}
+
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
 
+name <- "Kelly"
+object <- "Spagh"
+test3 <- describe_difference(name, object)
+
+object2 <- "Spaghetti"
+test4 <- describe_difference(object2, name)
+test5 <- describe_difference(name, object2)
